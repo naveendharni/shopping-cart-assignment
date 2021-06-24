59217a4779c51764c2b90a27a77703182ffbfadd
Naveen Dharni <naveen.dharni@gmail.com>
2021-06-24 17:28:28 +0530

    structure: remove old page folder

--- src/temp/Home/Home.styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import { breakPoints } from "components/organisms/LoginBody/LoginBody.styles";
-
-export const Container = styled.div`
-  margin: 90px 10rem 0;
-  @media (max-width: ${breakPoints.tabletBreak}) {
-    margin: 90px 0;
-  }
-`;
