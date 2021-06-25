d75b6710b24cea2d1623a01aeda1521c2a5ea14e
Naveen Dharni <naveen.dharni@gmail.com>
2021-06-25 17:31:32 +0530

    design: fix margin

--- src/components/pages/Product/Product.styles.js
+++ src/components/pages/Product/Product.styles.js
@@ -2,9 +2,9 @@ import styled from "styled-components";
 import { breakPoints } from "components/organisms/LoginBody/LoginBody.styles";
 
 export const Container = styled.div`
-  margin: 86px 10rem 0;
+  margin: 83px 10rem 0;
   position: relative;
   @media (max-width: ${breakPoints.tabletBreak}) {
-    margin: 86px 0;
+    margin: 75px 0;
   }
 `;
