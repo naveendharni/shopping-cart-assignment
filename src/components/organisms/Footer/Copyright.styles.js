37f95278a6ef6e674aa0a827e7f08d8a116bf61e
Naveen Dharni <naveen.dharni@gmail.com>
2021-06-25 12:47:56 +0530

    design: stick footer to bottom

--- src/components/organisms/Footer/Copyright.styles.js
+++ src/components/organisms/Footer/Copyright.styles.js
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 import { breakPoints } from "components/organisms/LoginBody/LoginBody.styles";
 
-export const Container = styled.div`
+export const Container = styled.footer`
   background: lightgray;
   padding: 0.8rem 1.5rem 0.8rem 15%;
   font-weight: 600;
   z-index: 2;
   position: relative;
+  margin-top: auto;
   @media (max-width: ${breakPoints.tabletBreak}) {
     padding: 0.8rem 0;
     text-align: center;
